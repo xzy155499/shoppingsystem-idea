f3aaa1b0ab78883b4363a09ed3e0e8d095e16b4f
购物项目 <2321259949@qq.com>
2020-12-21 10:34:47 +0800

    后端代码 项目架构

--- shoppingsystem-service/src/main/java/com/guigu/service/impl/MenuServiceImpl.java
+++ shoppingsystem-service/src/main/java/com/guigu/service/impl/MenuServiceImpl.java
@@ -1,8 +1,10 @@
 package com.guigu.service.impl;
 
+import com.github.pagehelper.PageHelper;
 import com.guigu.dao.MenuDao;
 import com.guigu.service.MenuService;
 import com.guigu.vo.MenuInfo;
+import com.guigu.vo.PageVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,23 +16,64 @@ public class MenuServiceImpl implements MenuService {
     MenuDao menuDao;
 
     @Override
+    public PageVo<MenuInfo> queryMenuByCond(MenuInfo menuInfo, int page, int rows) {
+        PageVo<MenuInfo> pageVo = new PageVo<MenuInfo>();
+        PageHelper.startPage(page,rows);
+
+        pageVo.setRows(menuDao.queryMenuByCond(menuInfo));
+        pageVo.setTotal(menuDao.queryMenuCountByCond(menuInfo));
+
+        return pageVo;
+    }
+
+    @Override
+    public int disabledMenu(MenuInfo menuInfo) {
+        return menuDao.disabledMenu(menuInfo);
+    }
+
+    @Override
     public List<MenuInfo> queryAllMenu(int emp_id, int nodeType) {
         //查询所有的父菜单  父节点为0  菜单类型为1
-        List<MenuInfo> menus = menuDao.queryMenuByPidAndNodeType(emp_id, 0, 1);
+        List<MenuInfo> menus = menuDao.queryMenuByEmpIdPidAndNodeType(emp_id, 0, 1);
 
         //将所有的父菜单的子菜单查询出来，绑定好
         for (MenuInfo menu : menus) {
-            List<MenuInfo> childMenus1 = menuDao.queryMenuByPidAndNodeType(emp_id, menu.getId().intValue(), 2);
+            List<MenuInfo> childMenus1 = menuDao.queryMenuByEmpIdPidAndNodeType(emp_id, menu.getId().intValue(), 2);
 
             menu.setChildMenu(childMenus1);
 
             if (nodeType == 3) {
                 for (MenuInfo menu2 : childMenus1) {
-                    List<MenuInfo> childMenus2 = menuDao.queryMenuByPidAndNodeType(emp_id, menu2.getId().intValue(), 3);
+                    List<MenuInfo> childMenus2 = menuDao.queryMenuByEmpIdPidAndNodeType(emp_id, menu2.getId().intValue(), 3);
                     menu2.setChildMenu(childMenus2);
                 }
             }
         }
         return menus;
+    }
+
+    @Override
+    public List<MenuInfo> queryAllTreeMenu(int nodeType) {
+        //查询所有的父菜单  父节点为0  菜单类型为1
+        List<MenuInfo> menus = menuDao.queryAllMenuByPidAndNodeType(0, 1);
+        //将所有的父菜单的子菜单查询出来，绑定好
+        for (MenuInfo menu : menus) {
+            List<MenuInfo> childMenus1 = menuDao.queryAllMenuByPidAndNodeType(menu.getId().intValue(), 2);
+
+            menu.setChildMenu(childMenus1);
+
+            if (nodeType == 3) {
+                for (MenuInfo menu2 : childMenus1) {
+                    List<MenuInfo> childMenus2 = menuDao.queryAllMenuByPidAndNodeType(menu2.getId().intValue(), 3);
+                    menu2.setChildMenu(childMenus2);
+                }
+            }
+        }
+        return menus;
+    }
+
+    @Override
+    public List<Integer> queryMenuIdByRid(int role_id) {
+        return menuDao.queryMenuIdByRid(role_id);
     }
 }
--- shoppingsystem-web/src/main/java/com/guigu/controller/MenuController.java
+++ shoppingsystem-web/src/main/java/com/guigu/controller/MenuController.java
@@ -2,11 +2,14 @@ package com.guigu.controller;
 
 
 import com.guigu.service.MenuService;
+import com.guigu.vo.EmpInfo;
 import com.guigu.vo.MenuInfo;
+import com.guigu.vo.PageVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
@@ -15,12 +18,44 @@ public class MenuController {
     @Autowired
     MenuService menuService;
 
-    //查询出所有的菜单信息(员工登录 不同的员工有不同的菜单信息)
+    //查询所有员工信息 分页
+    @RequestMapping("/queryAllMenus.action")
+    @ResponseBody
+    @CrossOrigin
+    public PageVo<MenuInfo> queryAllMenus(MenuInfo menuInfo,
+                                       @RequestParam(value = "page", defaultValue = "1") int page,
+                                       @RequestParam(value = "rows", defaultValue = "5") int rows) {
+        return menuService.queryMenuByCond(menuInfo, page, rows);
+    }
+
+    //查询所有员工信息 分页
+    @RequestMapping("/disabledMenu.action")
+    @ResponseBody
+    @CrossOrigin
+    public int disabledMenu(MenuInfo menuInfo) {
+        return menuService.disabledMenu(menuInfo);
+    }
+    //查询出所有的菜单信息(员工登录 不同的员工有不同的菜单信息) 导航菜单
     @CrossOrigin
     @RequestMapping("/queryAllMenu.action")
     @ResponseBody
-    public List<MenuInfo> queryAllMenu() {
-        return menuService.queryAllMenu(1,2);
+    public List<MenuInfo> queryAllMenu(Integer emp_id) {
+        return menuService.queryAllMenu(emp_id,2);
     }
 
+    //查询出所有的菜单信息
+    @CrossOrigin
+    @RequestMapping("/queryAllTreeMenu.action")
+    @ResponseBody
+    public List<MenuInfo> queryAllTreeMenu() {
+        return menuService.queryAllTreeMenu(2);
+    }
+
+    //根据角色id查询当前角色拥有的菜单
+    @CrossOrigin
+    @RequestMapping("/queryMenuIdByRid.action")
+    @ResponseBody
+    public List<Integer> queryMenuIdByRid(int role_id) {
+        return menuService.queryMenuIdByRid(role_id);
+    }
 }
